20ce665be7576e699a5cfb70fc5a85fcd827ae77
Saulo Costa <saulotarsobc@gmail.com>
2022-03-02 17:40:53 -0300

    a

--- msg/helpme.md
+++
@@ -1,3 +0,0 @@
-# Ajuda
-
-* [Python Write CSV File](https://www.pythontutorial.net/python-basics/python-write-csv-file/)
--- msg/mycsv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-
-def setCsv(fileName, data ):
-    with open(f'{fileName}.csv', 'w', encoding='UTF8', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
